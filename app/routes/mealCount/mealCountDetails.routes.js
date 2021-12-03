ac042f5074afdd3dd10657af2ac057ecaccbc40a
Fa-d <fsadakathussain@gmail.com>
2021-12-04 01:06:17 +0600

    bugs fixed

--- app/routes/mealCount/mealCountDetails.routes.js
+++ app/routes/mealCount/mealCountDetails.routes.js
@@ -2,11 +2,11 @@
 module.exports = app => {
     const { GETAllUsersMealCount,
         GETUserWiseMealCount,
-        UPDATEUserWiseMealCount },
-        getAllUsersTotalMealCount = require("../../controllers/mealCount/mealCountDetails.controller.js");
+        UPDATEUserWiseMealCount,getAllUsersTotalMealCount }
+         = require("../../controllers/mealCount/mealCountDetails.controller.js");
     app.get("/getAllUsersMealCount", GETAllUsersMealCount);
     app.post("/getUserWiseMealCount", GETUserWiseMealCount);
     app.post("/updateUserWiseMealCount", UPDATEUserWiseMealCount);
-    app.post("/getAllUsersTotalMealCount", getAllUsersTotalMealCount);
+    app.get("/getAllUsersTotalMealCount", getAllUsersTotalMealCount);
 };
 
